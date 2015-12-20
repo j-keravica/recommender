e9f816a1e84d9e1889ec07a5c4e3e6475afd466b
j-keravica <jelena.keravica@gmail.com>
2015-12-20 18:10:21 +0100

    Add Postgres config and update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,42 +2,40 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-# input = File.open(File.join(Rails.root, "db", "moviestxt.txt"))
-# input.each_line do |line|
-#   values = line.split(" ")
-#   length = values.length
 
-#   id = values.first.to_i
-#   popularity = values.last.to_i
-#   year = values[length - 2].delete('()').to_i
-#   title = values[1..length-3].join(" ");
 
-#   # puts "ID: #{id}, title: #{title}, year: #{year}, popularity: #{popularity}"
+input = File.open(File.join(Rails.root, "db", "moviestxt.txt"))
+input.each_line do |line|
+  values = line.split(" ")
+  length = values.length
 
-#   Movie.create(:movie_id => id, :title => title, :year => year, :movie_popularity => popularity)
-# end
+  id = values.first.to_i
+  popularity = values.last.to_i
+  year = values[length - 2].delete('()').to_i
+  title = values[1..length-3].join(" ");
 
-# input = File.open(File.join(Rails.root, "db", "tagstxt.txt"))
-# input.each_line do |line|
-#   values = line.split(" ")
-#   length = values.length
+  #puts "ID: #{id}, title: #{title}, year: #{year}, popularity: #{popularity}"
 
-#   id = values.first.to_i
-#   popularity = values.last.to_i
-#   name = values[1..length-2].join(" ")
+  #  Movie.create(:movie_id => id, :title => title, :year => year, :movie_popularity => popularity)
+end
 
-#   # puts "ID: #{id}, name: #{name}, popularity: #{popularity}"
+input = File.open(File.join(Rails.root, "db", "tagstxt.txt"))
+input.each_line do |line|
+  values = line.split(" ")
+  length = values.length
 
-#   Tag.create(:tag_id => id, :name => name, :tag_popularity => popularity)
-# end
+  id = values.first.to_i
+  popularity = values.last.to_i
+  name = values[1..length-2].join(" ")
+
+  puts "ID: #{id}, name: #{name}, popularity: #{popularity}"
+  if (popularity > 50)
+ #   Tag.create(:tag_id => id, :name => name, :tag_popularity => popularity)
+  end
+end
 
 input = File.open(File.join(Rails.root, "db", "tag_relevancetxt.txt"))
-i = 0
-t = 0
-tuples = []
+inserts = []
 
 input.each_line do |line|
 
@@ -48,20 +46,22 @@ input.each_line do |line|
   tag_id = values[1].to_i
   relevance = values.last.to_f
 
-  # puts "Movie ID: #{movie_id}, Tag ID: #{tag_id}, relevance: #{relevance}"
-
-  if (movie_id >= 107141)
-    tuples << MovieTag.new(:movie_id => movie_id, :tag_id => tag_id, :relevance => relevance)
-    i += 1
-  end
-
-  if i >= 1000
-    MovieTag.import tuples
-    t += i
-    puts "Imported #{t} tuples"
-    i = 0
-    tuples = []
+  tag_array = Tag.all.pluck(:id)
+  #puts "Movie ID: #{movie_id}, Tag ID: #{tag_id}, relevance: #{relevance}"
+  if (tag_array.include?(tag_id))
+  #  inserts.push "(#{movie_id}, #{tag_id}, #{relevance})"
   end
 end
 
-MovieTag.import tuples
+#sql = "INSERT INTO movie_tags (`movie_id`, `tag_id`, `relevance`) VALUES #{inserts.join(", ")}"
+
+
+
+
+# trebaće mi
+
+#inserts = []
+#TIMES.times do
+#  inserts.push "(#{movie_id}, #{tag_id}, #{relevance})"
+#end
+#sql = "INSERT INTO movie_tags (`movie_id`, `tag_id`, `relevance`) VALUES #{inserts.join(", ")}"
